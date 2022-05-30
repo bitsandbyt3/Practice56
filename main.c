940e0b1b9bb444877e2eebda1e343c7878b038dd
Patrik Schumann <patrik.schumann@smail.inf.h-brs.de>
2022-05-30 19:01:27 +0200

    Practice with input control

--- main.c
+++ main.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+    int itemAmount;
+    double itemPrice;
+
+    do {
+        printf("Input amount:");
+        scanf("%d", &itemAmount);
+
+        if (itemAmount < 0) {
+            printf("Amount must be a positive value!\n");
+            itemAmount = 0;
+        } else {
+            do {
+                printf("Input price:");
+                scanf("%lf", &itemPrice);
+
+                if(itemPrice < 0) {
+                    printf("Item price needs to be positive!\n");
+                } else if (itemPrice > 10) {
+                    printf("Item price needs to be less than 10 Euro!\n");
+                }
+            }while(itemPrice < 0 || itemPrice > 10);
+        }
+
+    }while(itemAmount == 0);
+
+    printf("Amount: %d\n", itemAmount);
+    printf("Price: %.2f\n", itemPrice);
 }
